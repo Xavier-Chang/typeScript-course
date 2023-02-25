f1a96b32798ef5b2eecac1cb1236d6f766591c84
Lai In Chang <lavender880331@gmail.com>
2023-02-25 11:29:35 +0100

    work with number, string and boolean

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
-function add(n1: number, n2: number) {
-  //JS --> run and throw the error, which can be prevented by TS
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  //JS --> run and throw the error, that's dynamic types (resolved at runtime)
+  //TS --> TS can know the problem eariler (staic types, set during development)
   // if (typeof n1 !== 'number' || typeof n2!== 'number'){
   //     throw new Error('Incorrect input!')
   // }
-  return n1 + n2;
+  const result = n1+ n2;
+  if (showResult === true) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
 }
 
-const number1 = "5";
+//TS type number, no difference of 5 / 5.0
+const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
