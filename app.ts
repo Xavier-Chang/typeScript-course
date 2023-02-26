269edc1e36cfe69a7cbeeb62f006814aab3e8b83
Lai In Chang <lavender880331@gmail.com>
2023-02-26 08:20:45 +0100

    know about array types

--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
     name: "Xaver",
-    age: 34
+    age: 34,
+    hobbies: ["sports", "cooking"]
 }
 
+let favouriteActivities: string[];
+favouriteActivities = ['sports']
+
 console.log(person.name);
+
+// person.hobbies.forEach((e)=>{
+//     console.log("hobby: ", e);
+// })
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase()); //TS know hobby is a string and toUpper.. can be used
+    // console.log(hobby.map()); //map is available on arr but not string
+}
