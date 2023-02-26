fa8bc09076a476de656bfb404a923bc012a1d763
Lai In Chang <lavender880331@gmail.com>
2023-02-26 07:40:37 +0100

    learn about type assignment and type inference

--- app.ts
+++ app.ts
@@ -13,9 +13,14 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 }
 
 //TS type number, no difference of 5 / 5.0
-const number1 = 5;
+let number1 = 5;
+//assign types of variable can do in this way:
+//let number1: number (assign value later)
+//number1 = '5'  --> IDE will show error here
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: '
+let resultPhrase = 'Result is: ' // TS think resultPhrase is a string
+// resultPhrase = "12" // OK
+// resultPhrase = 5 //not a string, error
 
 add(number1, number2, printResult, resultPhrase);
