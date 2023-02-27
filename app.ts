669bab47056fe61dd9fe8db8165c9c23c52fd966
Lai In Chang <lavender880331@gmail.com>
2023-02-27 10:22:20 +0100

    learn never type

--- app.ts
+++ app.ts
@@ -10,3 +10,9 @@ if (typeof userInput === "string") { //need extra check for unknown type
     userName = userInput;
 }
 
+//does not return void, if declare the type in void, it will crash the script
+function generateError(message: string, code: number): never {  //never return anything
+    throw {message: message, errorCode: code}
+}
+
+generateError('An error occurred!', 500);
