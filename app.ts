0466bf5de55bffd6c887e1bc1ca713bc7f727461
Lai In Chang <lavender880331@gmail.com>
2023-02-27 10:05:40 +0100

    not so clear about the cb type, need to review later

--- app.ts
+++ app.ts
@@ -9,6 +9,11 @@ function printResult(num: number): void { //return type is void, it doesn't retu
 //printResult(add(5, 12));
 //console.log(printResult(add(5, 12))); //undefinied
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
+}
+
 let combineValues: (a: number, b: number) => number;
  
 combineValues = add;
@@ -16,3 +21,7 @@ combineValues = add;
 //combineValues = printResult; //set type = function is not enough, as it can equal to other function
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+    console.log("CB here, print result..." + result);
+})
