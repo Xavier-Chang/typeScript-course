576729531171c1346a9c345e55c607e40898a6d1
Lai In Chang <lavender880331@gmail.com>
2023-02-28 14:41:51 +0100

    learn more about noUsedLocals, para, and return

--- TS Compiler/src/app.ts
+++ TS Compiler/src/app.ts
@@ -1,3 +1,5 @@
+let appId = "abc";
+
 //TS can't dive in the HTML file and see if the button is existed. So it may be null
 const button = document.querySelector("button")!; //! -> I make sure to TS that button is existed
 
@@ -12,10 +14,19 @@ const button = document.querySelector("button")!; //! -> I make sure to TS that 
 //     console.log("Clicked!!");
 //   });
 
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  } else {
+    return 0
+  }
+}
+
 function clickHandler(message: string) {
-    console.log('clicked' + message);
+  let unserName = "Xavier";
+  console.log("clicked" + message);
 }
 
 if (button) {
-    button.addEventListener('click', clickHandler.bind(null, "yor're welcome"));
+  button.addEventListener("click", clickHandler.bind(null, "yor're welcome"));
 }
