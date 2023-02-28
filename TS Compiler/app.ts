cb1763b628ab264a249abb62c5616c1ffed173e4
Lai In Chang <lavender880331@gmail.com>
2023-02-28 10:54:50 +0100

    set something in the lib of tsconfig.json

--- TS Compiler/app.ts
+++ TS Compiler/app.ts
@@ -1,3 +1,5 @@
-const userName = "Xavier Chang"
+const button = document.querySelector('button')!;
 
-console.log(userName);
+button.addEventListener('click', ()=> {
+    console.log('clicked!');
+})
