7aed663c13d0a94364a578b258a402b3caa054b9
Lai In Chang <lavender880331@gmail.com>
2023-02-28 14:31:26 +0100

    all about the strict option

--- TS Compiler/src/app.ts
+++ TS Compiler/src/app.ts
@@ -1,5 +1,21 @@
-const button = document.querySelector('button')!;
+//TS can't dive in the HTML file and see if the button is existed. So it may be null
+const button = document.querySelector("button")!; //! -> I make sure to TS that button is existed
 
-button.addEventListener('click', ()=> {
-    console.log('Clicked!!');
-})
+//runtime check
+// if (button) {
+//   button.addEventListener("click", () => {
+//     console.log("Clicked!!");
+//   });
+// }
+
+// button.addEventListener("click", () => {
+//     console.log("Clicked!!");
+//   });
+
+function clickHandler(message: string) {
+    console.log('clicked' + message);
+}
+
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, "yor're welcome"));
+}
