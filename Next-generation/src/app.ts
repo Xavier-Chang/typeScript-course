3d0717f31bc22067fd91d2a506a839b5fcf96fc4
Lai In Chang <lavender880331@gmail.com>
2023-03-01 17:24:28 +0100

    ... is very useful to pull all the element

--- Next-generation/src/app.ts
+++ Next-generation/src/app.ts
@@ -10,7 +10,7 @@ age = 29;
 //     return result;
 // }
 
-const add = (a: number, b: number) =>  a + b;
+const add = (a: number, b: number = 1) =>  a + b; //default argument -> b=1 if no assignment. But it can't set to a as it can't skip 
 
 console.log(add(2, 5));
 
@@ -22,4 +22,18 @@ if (button) {
     button.addEventListener('click', event => console.log(event));
 }
 
-printOutput(add(5, 2))
+printOutput(add(5))
+
+const hobbies = ["sport", "piano", "cooking"];
+// const activeHobbies = ['hiking', ...hobbies];
+const activeHobbies = ['hiking'];
+
+activeHobbies.push(...hobbies) //"..."" tell JS to pull out all the element
+console.log(activeHobbies);
+
+const person = {
+    name: 'Xavier',
+    age: 30
+}
+
+const copiedPerson = {...person}; //"..." pull all the key value pairs out of there
