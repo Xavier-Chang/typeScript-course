31612f04911e6dbeed5d1d021ea33698210be1ae
Lai In Chang <lavender880331@gmail.com>
2023-03-01 17:10:28 +0100

    review about arrow function and let & const

--- Next-generation/src/app.ts
+++ Next-generation/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+const userName = "Xavier"
+// userName = "XaC"
+let age = 30;
+
+age = 29;
+
+// function add(a: number, b: number) {
+//     var result;
+//     result = a + b;
+//     return result;
+// }
+
+const add = (a: number, b: number) =>  a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: string | number) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2))
