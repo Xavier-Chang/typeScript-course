43725c0638ebcfde3ba30d18ff056c81ecd5f31e
Lai In Chang <lavender880331@gmail.com>
2023-03-01 17:35:40 +0100

    learn about REST parameters

--- Next-generation/src/app.ts
+++ Next-generation/src/app.ts
@@ -1,4 +1,4 @@
-const userName = "Xavier"
+const userName = "Xavier";
 // userName = "XaC"
 let age = 30;
 
@@ -10,30 +10,40 @@ age = 29;
 //     return result;
 // }
 
-const add = (a: number, b: number = 1) =>  a + b; //default argument -> b=1 if no assignment. But it can't set to a as it can't skip 
+// const add = (a: number, b: number = 1) =>  a + b; //default argument -> b=1 if no assignment. But it can't set to a as it can't skip
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
-const printOutput: (a: string | number) => void = output => console.log(output);
+// const printOutput: (a: string | number) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-    button.addEventListener('click', event => console.log(event));
-}
+// if (button) {
+//     button.addEventListener('click', event => console.log(event));
+// }
 
-printOutput(add(5))
+// printOutput(add(5))
 
 const hobbies = ["sport", "piano", "cooking"];
 // const activeHobbies = ['hiking', ...hobbies];
-const activeHobbies = ['hiking'];
+const activeHobbies = ["hiking"];
 
-activeHobbies.push(...hobbies) //"..."" tell JS to pull out all the element
+activeHobbies.push(...hobbies); //"..."" tell JS to pull out all the element
 console.log(activeHobbies);
 
 const person = {
-    name: 'Xavier',
-    age: 30
-}
+  name: "Xavier",
+  age: 30,
+};
 
-const copiedPerson = {...person}; //"..." pull all the key value pairs out of there
+const copiedPerson = { ...person }; //"..." pull all the key value pairs out of there
+
+const add = (...numbers: number[]) => {
+  //REST parameters - ... merge all incoming parameters into an array
+  return numbers.reduce((acc, cum) => {
+    return acc + cum;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3, 7);
+console.log(addedNumbers);
