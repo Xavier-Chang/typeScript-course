8d0aa50470064bbb7521bd2c239e349b59309263
Lai In Chang <lavender880331@gmail.com>
2023-03-02 16:50:13 +0100

    learn about public and private in class

--- class_&_interfaces/src/app.ts
+++ class_&_interfaces/src/app.ts
@@ -1,5 +1,7 @@
 class Department {
-    name: string;
+    //JS just know public, TS supports private
+    public name: string; //public - default, acessible from outside
+    private employees: string[] = []; //modifier: private - only access inside the class
 
     constructor(n: string) {
         this.name = n;
@@ -8,13 +10,29 @@ class Department {
     describe(this: Department) { //When describe is excuted, refer to an instance that's based on the department class
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        //validation
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+//no validation
+// accounting.employees[2] = "Melody";// I can add employee outside the class method, not a good way. One uniform way
+
+accounting.addEmployee("Xavier");
+accounting.addEmployee("Winnie")
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'Finance', describe: accounting.describe}
+// const accountingCopy = { name: 'Finance', describe: accounting.describe}
 
-accountingCopy.describe();
+// accountingCopy.describe();
