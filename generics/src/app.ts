fa4b4ca44323567ab1161b6383ce210adabe0376
Lai In Chang <lavender880331@gmail.com>
2023-03-07 13:14:49 +0100

    learn about generics classes

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -19,8 +19,65 @@ function merge<T extends object , U extends object>(objA: T, objB: U) { //T has 
 //what generics do is like below
 // const mergeObj = merge<{name: string, hobbies: string[]}, {age:number}>({name: 'Xavier', hobbies: ['piano', 'jogging']}, {age: 35})
 const mergeObj = merge({name: 'Xavier', hobbies: ['piano', 'jogging']}, {age: 35})
-console.log(mergeObj); //TS don't get enough information originally, but now it know by generics
+// console.log(mergeObj); //TS don't get enough information originally, but now it know by generics
 
 //working with constraints - extends
 const mergeObjA = merge({name: 'Xavier', hobbies: ['piano', 'jogging']}, {age: 35}) //objB can't be 35
-console.log(mergeObjA)
+// console.log(mergeObjA)
+
+interface Lengthy {
+    length: number
+}
+
+function courtAndDescribe<T extends Lengthy>(element: T): [T, string] { //the fact is that it has length property, whatever it's string or array. But number is not OK as it has no length
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 elements.'
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.'
+    }
+    return [element, descriptionText]
+}
+
+// console.log(courtAndDescribe(['sport', 'cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value: ' + obj[key]; //how can TS know that the key is exactly exist in the object, by setting keyof
+}
+
+extractAndConvert({name: 'Xavier', age: 30}, 'age')
+
+class DataStorage<T extends string | number | boolean> { //generic type no need to limit the type here
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data]
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Xavier')
+textStorage.addItem('Chang')
+textStorage.removeItem('Chang')
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+
+// objStorage.addItem({name: 'Xavier'})
+// objStorage.addItem({name: 'Chang'});
+// // ...
+// objStorage.removeItem({name: 'Xavier'}) //reference address issue, it's a new object. just remove the last element
+// console.log(objStorage.getItems());
