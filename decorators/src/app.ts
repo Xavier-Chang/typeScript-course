4da5b1cc123c95d8de3800685c2eb8d61d6d2486
Lai In Chang <lavender880331@gmail.com>
2023-03-09 12:38:02 +0100

    learn Decorator for Validation

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -19,15 +19,18 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   //not actually return anything here
-  return function<T extends {new(...args: any[]): {name: string}}> (originalConstructor: T) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
     // _:function "_" --> I aware of it but won't use it
 
-    return class extends originalConstructor { //replace the old class
+    return class extends originalConstructor {
+      //replace the old class
       constructor(..._: any[]) {
         super();
         console.log("rendering template");
         const hookEl = document.getElementById(hookId);
-        
+
         if (hookEl) {
           hookEl.innerHTML = template;
           hookEl.querySelector("h1")!.textContent = this.name;
@@ -111,6 +114,9 @@ class Product {
   }
 }
 
+//******************//
+//decorator example - Autobind
+
 function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
@@ -119,13 +125,13 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     get() {
       const boundFn = originalMethod.bind(this); //this refer to the object belongs
       return boundFn;
-    }    
-  }
+    },
+  };
   return adjDescriptor;
 }
 
 class Printer {
-  message = 'This works!';
+  message = "This works!";
 
   @Autobind //auto bind to the message, no need to add in the event listener
   showMessage() {
@@ -133,8 +139,93 @@ class Printer {
   }
 }
 
-const p = new Printer()
+const p = new Printer();
 
-const button = document.querySelector('button') as HTMLElement;
+const button = document.querySelector("button") as HTMLElement;
 //button.addEventListener('click', p.showMessage)//it's nor work as this. is refer to eventlistener but not message
-button.addEventListener('click', p.showMessage) //bind to the class
+button.addEventListener("click", p.showMessage); //bind to the class
+
+//******************//
+//decorator example - Decorator for Validation
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; //['required', 'positive']
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    //Course constructor name
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    //Course constructor name
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    console.log(prop);
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault;
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value; //+ -- convert to number
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert("invalid input, please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
