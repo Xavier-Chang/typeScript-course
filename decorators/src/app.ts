82fadb2f61c8cac9c7b9764cb202513e58e6b0d6
Lai In Chang <lavender880331@gmail.com>
2023-03-09 11:27:37 +0100

    learn autobind

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -110,3 +110,31 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this); //this refer to the object belongs
+      return boundFn;
+    }    
+  }
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind //auto bind to the message, no need to add in the event listener
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer()
+
+const button = document.querySelector('button') as HTMLElement;
+//button.addEventListener('click', p.showMessage)//it's nor work as this. is refer to eventlistener but not message
+button.addEventListener('click', p.showMessage) //bind to the class
